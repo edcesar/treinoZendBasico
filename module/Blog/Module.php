fe7a0313d1e175bdfe69da7b9cdaa9fb9287b082
Ed Cesar <edcesar@edcesar.com>
2017-02-06 12:01:22 +0000

    start

---
+++ module/Blog/Module.php
@@ -0,0 +1,16 @@
+<?php
+namespace Blog;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+	public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
+
+
+
---
+++ module/Blog/config/module.config.php
@@ -0,0 +1,45 @@
+<?php
+namespace Blog;
+
+use Blog\Controller\BlogController;
+
+return [
+	 'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route'    => '/blog',
+                    'defaults' => array(
+                        'controller' => BlogController::class,
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+          ),
+     ),
+
+      'controllers' => array(
+        'invokables' => array(
+            BlogController::class => BlogController::class
+        ),
+    ),
+
+      'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'blob/index/index' => __DIR__ . '/../view/blob/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+];
+
