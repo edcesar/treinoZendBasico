c6918b6d6b47f711dccf316fe9466d1957ba990d
Ed Cesar <edcesar@edcesar.com>
2017-02-06 19:20:54 +0000

    trabalhando com ZendSql

--- module/Blog/src/Controller/BlogController.php
+++ module/Blog/src/Controller/BlogController.php
@@ -4,6 +4,7 @@ namespace Blog\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Blog\Categories;
+use Zend\Db\Sql\Sql;
 
 
 class BlogController extends AbstractActionController
@@ -15,11 +16,16 @@ class BlogController extends AbstractActionController
 	//	$categorias = $categorias->getCategories()[0]->getName();
 		$categorias = $categorias->getCategories();;
 		
-		
+		$adapter = $this->getServiceLocator()->get('Zend\Db\Adapter\Adapter');
+
+		$sql = new Sql($adapter);
+		$select = $sql->select()->from('categories');
+		$stmt = $sql->prepareStatementForSqlObject($select);
+		$result = $stmt->execute();
 
 	//	$categorias = new \Blog\CategoriesFactory();
 	//	$categorias = $categorias->createService($this->getServiceLocator());
 	
-		return new ViewModel(['categorias' => $categorias]);
+		return new ViewModel(['categorias' => $categorias, 'result' => $result]);
 	}
 }
